0c01fcbe2b371b375edefac34916db4695f9a0ee
Meku-nint <mw908334@gmail.com>
2025-11-27 15:23:31 +0300

    the first c# program

---
+++ Csharp.cs
@@ -0,0 +1,9 @@
+using System;
+class Csharp {
+     string name="Mekunint";
+    static void Main () {
+        Csharp user =new Csharp ();
+        string userName=user.name;
+        Console.WriteLine("Hello ,"+userName);
+    }
+}
